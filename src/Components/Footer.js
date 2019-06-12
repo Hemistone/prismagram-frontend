159a711f6765398b8a3c65215e0501f4b74512ae
Pansok Hwang <kei6040@gmail.com>
2019-06-12 23:04:19 +0900

    Checked until 7.4, finished until 7.5

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,6 +8,7 @@ const Footer = styled.footer`
   text-transformation: uppercase;
   font-weight: 600;
   font-size: 12px;
+  margin: 50px 0px;
 `;
 
 const List = styled.ul`
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -21,6 +21,7 @@ const Header = styled.header`
   justify-content: center;
   align-items: center;
   padding: 25px 0px;
+  z-index: 2;
 `;
 
 const HeaderWrapper = styled.div`
@@ -88,7 +89,11 @@ export default withRouter(({ history }) => {
         </HeaderColumn>
         <HeaderColumn>
           <form onSubmit={onSearchSubmit}>
-            <SearchInput {...search} placeholder="Search" />
+            <SearchInput
+              value={search.value}
+              onChange={search.onChange}
+              placeholder="Search"
+            />
           </form>
         </HeaderColumn>
         <HeaderColumn>
